7022e4066208ec152de712e0b3d7a3efca18518a
shenxingchao <864318466@qq.com>
2021-09-06 15:41:35 +0800

    1.完成文章编辑页面

--- src/views/article/article-edit.vue
+++ src/views/article/article-edit.vue
@@ -1,16 +1,195 @@
 <template>
-  <div>333</div>
+  <div class="app-container">
+    <el-card shadow="hover">
+      <el-row type="flex" justify="left">
+        <el-col :xs="24" :md="16">
+          <el-form ref="ruleFormRef" :rules="rules" :model="ruleForm" label-position="right" label-width="150px">
+            <el-form-item label="标题" prop="title">
+              <el-input v-model="ruleForm.title" placeholder="标题" />
+            </el-form-item>
+            <el-form-item label="图片" prop="image">
+              <upload :file="ruleForm.image" @handleUploadSuccess="handleUploadSuccess($event)"
+                      @handleDeleteFile="ruleForm.image = ''">
+              </upload>
+            </el-form-item>
+            <el-form-item label="图片列表" prop="image_list">
+              <upload :files="ruleForm.image_list" multiple
+                      @handleUploadMultipleSuccess="handleUploadMultipleSuccess($event)"
+                      @handleClickDeleteMultiple="ruleForm.image_list = $event">
+              </upload>
+            </el-form-item>
+            <el-form-item label="作者" prop="author">
+              <el-input v-model="ruleForm.author" placeholder="作者" />
+            </el-form-item>
+            <el-form-item label="详情" prop="detail">
+              <editor :value="ruleForm.detail" @change="handleChangeEditor($event)">
+              </editor>
+            </el-form-item>
+            <el-form-item label="推荐" prop="recommend">
+              <el-switch v-model="ruleForm.recommend" active-color="#13ce66" inactive-color="#ff4949">
+              </el-switch>
+            </el-form-item>
+            <el-form-item label="置顶" prop="top">
+              <el-switch v-model="ruleForm.top" active-color="#13ce66" inactive-color="#ff4949">
+              </el-switch>
+            </el-form-item>
+            <el-form-item label="状态" prop="">
+              <el-switch v-model="ruleForm.status" active-color="#13ce66" inactive-color="#ff4949">
+              </el-switch>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="success" @click="submitForm()">保存</el-button>
+              <el-button @click="resetForm()">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
 </template>
 <script>
-import { defineComponent } from 'vue'
-import { useRouter } from 'vue-router'
+import {
+  defineComponent,
+  getCurrentInstance,
+  onMounted,
+  reactive,
+  ref,
+  toRefs,
+} from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import { articleEdit, articleDetail } from '@/api/article'
+import Upload from '@/components/Upload/index.vue'
+import Editor from '@/components/TinymceEditor/index.vue'
 
 export default defineComponent({
-  components: {},
+  name: 'ArticleEdit',
+  components: {
+    Upload,
+    Editor,
+  },
   setup() {
-    const router = useRouter()
+    //当前组件实例
+    const internalInstance = getCurrentInstance()
 
-    return { router }
+    //访问 globalProperties
+    const global = internalInstance?.appContext.config.globalProperties
+
+    //定义router
+    const $router = useRouter()
+    const $route = useRoute()
+
+    //数据对象
+    let data = reactive({
+      ruleForm: {
+        id: $route.query.id,
+        title: '',
+        image: '',
+        image_list: [],
+        author: '',
+        type: '',
+        detail: '',
+        recommend: true,
+        top: true,
+        status: true,
+      },
+      rules: {
+        title: [
+          {
+            required: true,
+            message: '请输入标题',
+            trigger: 'blur',
+          },
+        ],
+        image: [
+          {
+            required: true,
+            message: '请上传图片',
+          },
+        ],
+        image_list: [
+          {
+            required: true,
+            message: '请上传图片列表',
+          },
+        ],
+        author: [
+          {
+            required: true,
+            message: '请输入作者',
+            trigger: 'blur',
+          },
+        ],
+        detail: [
+          {
+            required: true,
+            message: '请输入详情',
+          },
+        ],
+      },
+    })
+
+    const ruleFormRef = ref(null)
+
+    onMounted(() => {
+      getArticleDetail()
+    })
+
+    const getArticleDetail = () => {
+      articleDetail({ id: data.ruleForm.id })
+        .then((res) => {
+          data.ruleForm = res.data
+        })
+        .catch(() => {})
+    }
+
+    const submitForm = () => {
+      ruleFormRef.value.validate((valid) => {
+        if (valid) {
+          articleEdit(data.ruleForm)
+            .then((res) => {
+              global.$message({
+                message: '保存成功',
+                type: 'success',
+                onClose: function () {
+                  $router.push('/article/article-list')
+                },
+              })
+            })
+            .catch(() => {})
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
+
+    const resetForm = () => {
+      ruleFormRef.value.resetFields()
+    }
+
+    const handleUploadSuccess = (imgUrl) => {
+      data.ruleForm.image = imgUrl
+      ruleFormRef.value.clearValidate('image')
+    }
+
+    const handleUploadMultipleSuccess = (imgUrlList) => {
+      data.ruleForm.image_list = imgUrlList
+      ruleFormRef.value.clearValidate('image_list')
+    }
+
+    const handleChangeEditor = (content) => {
+      data.ruleForm.detail = content
+    }
+
+    return {
+      ...toRefs(data),
+      ruleFormRef,
+      submitForm,
+      resetForm,
+      handleUploadSuccess,
+      handleUploadMultipleSuccess,
+      handleChangeEditor,
+    }
   },
 })
 </script>
