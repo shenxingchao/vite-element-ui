2855d9b5ef14191b1075d7d2c3cdc16de29c07a5
shenxingchao <864318466@qq.com>
2021-09-06 16:31:25 +0800

    1.修改一些标题

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'vue-admin-elementui'
+const title = defaultSettings.title || 'vue-element-ui'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
