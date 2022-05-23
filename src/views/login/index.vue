c287f40e12a56da2cd0d2aea715dd967f8ab2d78
shenxingchao <864318466@qq.com>
2022-05-23 10:22:17 +0800

    [update]1.升级依赖

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -157,8 +157,16 @@ $cursor: $theme;
     }
   }
 
+  .el-form-item.is-error .el-input__wrapper {
+    width: 100%;
+  }
+  .el-input__wrapper,
+  .el-form-item.is-error .el-input__wrapper {
+    box-shadow: none;
+  }
+
   .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
+    // border: 1px solid rgba(255, 255, 255, 0.1);
     background: rgba(0, 0, 0, 0.1);
     border-radius: 5px;
     color: #454545;
